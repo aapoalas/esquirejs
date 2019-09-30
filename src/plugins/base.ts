4711df864f7f09319a6da0b82f79a2c8b080ad29
Aapo Alasuutari <aapo.alasuutari@valmet.com>
2019-09-30 12:27:06 +0300

    Change copyright to original work copyright

--- src/plugins/base.ts
+++ src/plugins/base.ts
@@ -1,12 +1,5 @@
-/* Copyright 2019 Valmet Automation Inc.
- *
- * This document is the exclusive intellectual property of Valmet Automation Inc.
- * and/or its subsidiaries (collectively "Valmet Automation") and is furnished
- * solely for operating and maintaining the supplied equipment and/or software.
- * Use of the document for any other project or purpose is prohibited.
- * All copyrights to the document are reserved by Valmet Automation. Accordingly,
- * the document or the information contained therein shall not (whether partly or entirely)
- * be reproduced, copied or disclosed to a third party without prior written consent of Valmet Automation.
+/**
+ * Original Work Copyright 2019 Valmet Automation Inc.
  */
 
 import { ImportJSOptions } from "../options";
