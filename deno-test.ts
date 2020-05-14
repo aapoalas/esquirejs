26a13007615b6fba396d383bc12d3666df29c70e
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2020-05-14 22:30:55 +0300

    Remove deno-test.ts and add .vscode ignore

--- deno-test.ts
+++
@@ -1,8 +0,0 @@
-import { defineBasePlugin, definePlugin, load, plugins } from "https://raw.githubusercontent.com/aapoalas/esquirejs/deno/index.ts";
-
-defineBasePlugin(plugins.base);
-definePlugin("json", plugins.json);
-definePlugin("text", plugins.text);
-
-load("text!https://raw.githubusercontent.com/aapoalas/esquirejs/deno/index.ts");
-
